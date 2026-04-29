a7ecb8af002b9b27ce12dfb6770d5e8bb9d5d9b9
AdarshPadhya <adipadhya@gmail.com>
2026-04-29 11:36:45 +0930

    added a class for GUIApp

--- gui_app.py
+++ gui_app.py
@@ -4,3 +4,20 @@ from PIL import Image, ImageTk  # for handling the images
 
 from image_processor import ImageProcessor  # custom file for processing image
 from game_logic import GameLogic  # custom file for game logic
+
+class GUIApp:
+    def __init__(self):
+        self.root = tk.Tk()  # creating main window
+        self.root.title("Spot the Difference Game")  # setting title
+
+        self.processor = ImageProcessor()  # object for processing images
+        self.logic = None  # game logic will be assigned later
+
+        self.original_photo = None  # store original image
+        self.modified_photo = None  # store modified image
+
+        self.left_x = 50  # x position for left image
+        self.right_x = 550  # x position for right image
+        self.image_y = 120  # y position for both images
+
+        self.create_widgets()  # calling function to create UI
