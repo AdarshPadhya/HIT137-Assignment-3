0e425ec4a5bdca56e3f788b1aa9a5a8ecf49d04c
AdarshPadhya <adipadhya@gmail.com>
2026-04-29 11:27:08 +0930

    added new lines of code

---
+++ gui_app.py
@@ -0,0 +1,6 @@
+import tkinter as tk  # for GUI
+from tkinter import filedialog, messagebox  # for file open dialog and popup messages
+from PIL import Image, ImageTk  # for handling the images
+
+from image_processor import ImageProcessor  # custom file for processing image
+from game_logic import GameLogic  # custom file for game logic
